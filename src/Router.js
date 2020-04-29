375d746e5bafacf78dde6f659cfff6c6cdc66007
Richard-97 <richard.rusnak97@gmail.com>
2020-04-29 23:04:14 +0200

    heroku

--- src/Router.js
+++ src/Router.js
@@ -3,8 +3,8 @@ import { BrowserRouter as Router, Switch } from 'react-router-dom';
 import App from './App';
 import auth from './utils/auth';
 
-//const api = 'http://localhost:5000'
-const api = 'https://diplomovka-be.herokuapp.com'
+const api = 'http://88.212.50.96:5000'
+//const api = 'http://192.168.10.150:5000'
 const nodejsApi = 'https://nodejs-be.herokuapp.com';
 //const nodejsApi = 'http://localhost:3000'
 
